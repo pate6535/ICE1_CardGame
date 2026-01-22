8476468190d29c1f4bf83faca55c277ea337d54d
pate6535 <pate6535@sheridancollege.ca>
2026-01-21 20:57:50 -0500

    student project version 3

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -33,6 +33,10 @@ public class CardTrick {
             c.setSuit(Card.SUITS[rand.nextInt(4)]);//c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             
             magicHand[i]=c;
+        } 
+        
+        for (int i = 0; i < magicHand.length; i++) {
+            System.out.println(magicHand[i].getSuit() + " " + magicHand[i].getValue());
         }
         
         //insert code to ask the user for Card value and suit, create their card
@@ -40,10 +44,12 @@ public class CardTrick {
         //Then report the result here
         // add one luckcard hard code 2,clubs  
         
-
+        
     Card luckyCard= new Card();
     luckyCard.setValue(7);      
-    luckyCard.setSuit("Hearts");          
+    luckyCard.setSuit("Hearts");   
+    
+    
         
 
     boolean found = false;
